f9736ea7ba1b501b8acf0d2a4db206a57f3c5880
常骏 <changjun.zl@alibaba-inc.com>
2021-01-29 11:06:14 +0800

    modify rds example code

---
+++ src/examples/java/com/aliyun/encryptionsdk/examples/rds/EncryptionConverter.java
@@ -0,0 +1,75 @@
+/**
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.aliyun.encryptionsdk.examples.rds;
+
+import com.aliyun.encryptionsdk.AliyunConfig;
+import com.aliyun.encryptionsdk.AliyunCrypto;
+import com.aliyun.encryptionsdk.cache.LocalDataKeyMaterialCache;
+import com.aliyun.encryptionsdk.ckm.CachingCryptoKeyManager;
+import com.aliyun.encryptionsdk.ckm.CryptoKeyManager;
+import com.aliyun.encryptionsdk.exception.InvalidAlgorithmException;
+import com.aliyun.encryptionsdk.exception.UnFoundDataKeyException;
+import com.aliyun.encryptionsdk.model.CryptoResult;
+import com.aliyun.encryptionsdk.provider.BaseDataKeyProvider;
+import com.aliyun.encryptionsdk.provider.dataKey.DefaultDataKeyProvider;
+
+import javax.persistence.AttributeConverter;
+import javax.persistence.Converter;
+import java.nio.charset.StandardCharsets;
+import java.util.Base64;
+import java.util.Collections;
+
+@Converter
+public class EncryptionConverter implements AttributeConverter<String, String> {
+    private static String ACCESS_KEY_ID = "<AccessKeyId>";
+    private static String ACCESS_KEY_SECRET = "<AccessKeySecret>";
+    private static String CMK_ARN = "acs:kms:RegionId:UserId:key/CmkId";
+    private static AliyunConfig config;
+    static {
+        config = new AliyunConfig();
+        config.withAccessKey(ACCESS_KEY_ID, ACCESS_KEY_SECRET);
+    }
+
+    @Override
+    public String convertToDatabaseColumn(String plainText) {
+        BaseDataKeyProvider dataKeyProvider = new DefaultDataKeyProvider(CMK_ARN);
+        AliyunCrypto crypto = new AliyunCrypto(config);
+
+        try {
+            CryptoResult<byte[]> encryptResult = crypto.encrypt(dataKeyProvider, plainText.getBytes(StandardCharsets.UTF_8), Collections.singletonMap("sample", "context"));
+            return Base64.getEncoder().encodeToString(encryptResult.getResult());
+        } catch (InvalidAlgorithmException e) {
+            System.out.println("Failed.");
+            System.out.println("Error message: " + e.getMessage());
+        }
+        return null;
+    }
+
+    @Override
+    public String convertToEntityAttribute(String cipherText) {
+        BaseDataKeyProvider dataKeyProvider = new DefaultDataKeyProvider(CMK_ARN);
+        AliyunCrypto crypto = new AliyunCrypto(config);
+        // *** 设置缓存ckm ***
+        CryptoKeyManager ckm = new CachingCryptoKeyManager(new LocalDataKeyMaterialCache());
+        crypto.setCryptoKeyManager(ckm);
+        try {
+            CryptoResult<byte[]> decryptResult = crypto.decrypt(dataKeyProvider, Base64.getDecoder().decode(cipherText));
+            return new String(decryptResult.getResult(), StandardCharsets.UTF_8);
+        } catch (InvalidAlgorithmException | UnFoundDataKeyException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+}
